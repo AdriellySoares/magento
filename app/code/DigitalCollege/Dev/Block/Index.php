6604b98d7789bd16aa4eff7b449e9e27bbadf472
Adrielly <113360657+AdriellySoares@users.noreply.github.com>
2022-12-14 10:44:13 -0300

    atualização dos arquivos magento

--- app/code/DigitalCollege/Dev/Block/Index.php
+++ app/code/DigitalCollege/Dev/Block/Index.php
@@ -9,18 +9,18 @@ class Index extends \Magento\Framework\View\Element\Template
 
     protected $digitalFactory;
 
-    public function __construct(\Magento\Framework\View\Element\Template\Context $context, 
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context,
     DigitalFactory $digitalFactory) {
         $this->digitalFactory = $digitalFactory;
         return parent::__construct($context);
     }
-    
+
     public function getDados(){
         $dc = $this->digitalFactory->create()->getCollection();
         return $dc->getData();
     }
 
-    public function saveData(){
+    public function execute(){
         $dc = $this->digitalFactory->create();
         $dc->setData([
             'author_name' => 'Eu',
