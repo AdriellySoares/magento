2b97468c01c6c43035058312483c3a303f513087
Adrielly <113360657+AdriellySoares@users.noreply.github.com>
2022-12-21 11:56:28 -0300

    Add files via upload

--- app/code/DigitalCollege/Dev/Api/PostManagementInterface.php
+++ app/code/DigitalCollege/Dev/Api/PostManagementInterface.php
@@ -7,9 +7,9 @@ interface PostManagementInterface {
 
     /**
      * GET for Post api
-     * @param string $param
+
      * @return string
      */
 
-    public function getPost($param);
+    public function getPost();
 }
